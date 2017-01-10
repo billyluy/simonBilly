9b76b002176db23a477fce069aa94e8a7c5def95
Student 6 <Student 6@10.8.33.100>
2017-01-10 13:07:46 -0500

    partnerCode

---
+++ SimonBilly/src/partnerCodeHerePlease/Progress.java
@@ -0,0 +1,21 @@
+package partnerCodeHerePlease;
+
+import java.awt.Graphics2D;
+
+import gui.components.Component;
+import simonBilly.ProgressInterfaceBillyLuy;
+
+public class Progress extends Component implements ProgressInterfaceBillyLuy {
+
+	public Progress(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(Graphics2D arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
