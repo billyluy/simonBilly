c5b0d4add75db308853f5b9e9cc89bbcff7595fc
MatthewNguyenRAZR <matthewpdn@gmail.com>
2017-01-12 20:38:09 -0500

    Direct Edit

--- SimonBilly/src/partnerCodeHerePlease/Progress.java
+++ SimonBilly/src/partnerCodeHerePlease/Progress.java
@@ -1,6 +1,9 @@
 package partnerCodeHerePlease;
 
+import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import gui.components.Component;
 import simonBilly.ProgressInterfaceBillyLuy;
@@ -17,9 +20,27 @@ public class Progress extends Component implements ProgressInterfaceBillyLuy {
 	}
 
 	@Override 
-	public void update(Graphics2D arg0) {
-		// TODO Auto-generated method stub
- 
+	public void update(Graphics2D g) {
+		g = clear();
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		FontMetrics fm = g.getFontMetrics();
+		if(gameOver){
+			g.setColor(Color.RED);
+			g.fillRect(0, 0, width, height);
+			g.setColor(Color.black);
+			String text = "Game Over";
+			g.drawString(text, 40,20);
+			g.drawString(sequence, 40, 40);
+		}else{
+			g.setColor(Color.pink);
+			g.fillRect(0, 0, width, height);
+			g.setColor(Color.black);
+			g.drawRect(0, 0, width-1, height-1);
+			if(round !=null && sequence != null){
+				g.drawString(round, 40, 20);
+				g.drawString(sequence, 40, 40);
+			}
+		}
 	}
 
 }
